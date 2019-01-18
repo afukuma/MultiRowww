49fe22f8c086b3f99c065ecedf3e23ca381ec41f
Akihiko FUKUMA <afukuma@me.com>
2019-01-18 22:47:23 +0900

    どや

---
+++ MultiRowww/MultiRowww/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MultiRowww
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(410, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(321, 287);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellEnter);
+            this.dataGridView1.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellLeave);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 30);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 21;
+            this.dataGridView2.Size = new System.Drawing.Size(392, 287);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "元データ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(410, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "２段";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 332);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MultiRowww/MultiRowww/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiRowww
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 元データ
+        /// </summary>
+        public class sData
+        {
+            public int key { get; set; }
+            public int subkey1 { get; set; }
+            public int subkey2 { get; set; }
+            public string moji1 { get; set; }
+            public string moji2 { get; set; }
+        }
+        /// <summary>
+        /// 表示用データ
+        /// </summary>
+        public class sDisp
+        {
+            public int key { get; set; }            //隠しキー
+            public string dispkey { get; set; }     //表示用キー
+            public int subkey { get; set; }
+            public string moji { get; set; }
+        }
+
+
+        //dataGrid用設定
+        private DataGridViewCellStyle defaultCellStyle;     //デフォルトのセルスタイル
+        private DataGridViewCellStyle mouseCellStyle;       //マウスポインタの下にあるセルのセルスタイル
+
+
+
+        private List<sData> lData = new List<sData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //テストデータ作成
+
+            for (int i = 0; i < 300; i++)
+            {
+                sData wData = new sData();
+                wData.key = i;
+                wData.subkey1 = i + 100;
+                wData.subkey2 = i + 200;
+                wData.moji1 = (i + 100).ToString() + "ほげ";
+                wData.moji2 = (i + 200).ToString() + "ほげ";
+
+                lData.Add(wData);
+            }
+            //元データをそのまま表示
+            dataGridView2.DataSource = lData;
+            dataGridView2.AutoResizeColumns();
+
+            // デフォルトのセルスタイルの設定
+            this.defaultCellStyle = new DataGridViewCellStyle();
+            //現在のセルのセルスタイルの設定
+            this.mouseCellStyle = new DataGridViewCellStyle();
+            this.mouseCellStyle.BackColor = Color.LightGreen;
+            this.mouseCellStyle.SelectionBackColor = Color.Green;
+
+            List<sDisp> lDisp = new List<sDisp>();
+            sDisp wDisp;
+            foreach (sData wData in lData)
+            {
+                //１行を２行に変換する
+                wDisp = new sDisp();
+                wDisp.key = wData.key;
+                wDisp.dispkey = wData.key.ToString();  //表示用キーは１段目のみに表示する
+                wDisp.subkey = wData.subkey1;
+                wDisp.moji = wData.moji1;
+                lDisp.Add(wDisp);
+
+                wDisp = new sDisp();
+                wDisp.key = wData.key;
+                wDisp.subkey = wData.subkey2;
+                wDisp.moji = wData.moji2;
+                lDisp.Add(wDisp);
+            }
+            //分解後のデータを表示
+            dataGridView1.DataSource = lDisp;
+            //dataGridView1.Columns[0].Width = 10;
+            dataGridView1.AutoResizeColumns();
+            ColorAllReset();
+        }
+
+
+        //DataGridView1のCellEnterイベントハンドラ
+        private void DataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            //ヘッダー以外のセル
+            if (e.ColumnIndex >= 0 && e.RowIndex >= 0)
+            {
+                DataGridView dgv = (DataGridView)sender;
+
+                //ペアとなるRow
+                int pearRow = PearRowIndex(e.RowIndex);
+
+                for (int i = 1; i < dgv.Columns.Count; i++)
+                {
+                    dgv[i, e.RowIndex].Style = this.mouseCellStyle;
+                    if (pearRow >= 0) dgv[i, pearRow].Style = this.mouseCellStyle;
+                }
+
+                Text = PearRowIndex(e.RowIndex).ToString();
+            }
+        }
+
+        //DataGridView1のCellLeaveイベントハンドラ
+        private void DataGridView1_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            /*
+            //ヘッダー以外のセル
+            if (e.ColumnIndex >= 0 && e.RowIndex >= 0)
+            {
+                DataGridView dgv = (DataGridView)sender;
+                for (int i = 1; i < dgv.Columns.Count; i++)
+                {
+                    dgv[i, e.RowIndex].Style = this.defaultCellStyle;
+                }
+            }
+            */
+            ColorAllReset();
+        }
+
+
+        //全部色リセット
+        private void ColorAllReset()
+        {
+            for (int c = 1; c < dataGridView1.Columns.Count; c++)
+            {
+                for (int r = 0; r < dataGridView1.Rows.Count; r++)
+                {
+                    dataGridView1[c, r].Style = this.defaultCellStyle;
+                }
+            }
+        }
+
+        //入力RowIndexをもとに、ペアとなる行を検索してペアRowIndexを返す
+        private int PearRowIndex(int rowIndex)
+        {
+            //ヘッダー以外のセル
+            DataGridView dgv = dataGridView1;
+
+            //選択行の先頭列に隠しキーがあるのでそれを取得する
+            int wKey = (int)dgv[0, rowIndex].Value;
+
+            //上から同一キーを探す。ただし入力パラメータは除く
+            for (int r = 0; r < dgv.Rows.Count; r++)
+            {
+                if (r != rowIndex)
+                {
+                    if (wKey == (int)dgv[0, r].Value)
+                    {
+                        return r;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+
+    }
+}
